c9604cbd3cd55d4e5c3f171bfc6012ad52606d62
islam.ar <islam.ahmed@approcks.com>
2022-05-31 15:22:45 +0200

    Album details screen UI

---
+++ app/src/main/java/com/islam/music/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.islam.music.common
+
+
+const val TIME_OUT_IN_SECONDS = 15L
+const val IMAGE_SIZE_MULTIPLIER = 0.1f
--- app/src/main/java/com/islam/music/features/album_details/AlbumDetailsFragment.kt
+++ app/src/main/java/com/islam/music/features/album_details/AlbumDetailsFragment.kt
@@ -3,9 +3,13 @@ package com.islam.music.features.album_details
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.islam.music.R
 import com.islam.music.common.BaseFragment
+import com.islam.music.common.IMAGE_SIZE_MULTIPLIER
 import com.islam.music.databinding.FragmentAlbumDetailsBinding
+import com.islam.music.features.main_screen.domain.entites.Album
+import com.islam.music.features.main_screen.presentation.view.AlbumsAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,8 +19,38 @@ class AlbumDetailsFragment : BaseFragment<FragmentAlbumDetailsBinding>() {
         get() = FragmentAlbumDetailsBinding::inflate
     override var screenTitle = R.string.album_details_screen_title
 
-    override fun setupOnViewCreated(view: View) {
+    private var list: MutableList<Album> = mutableListOf()
 
+    private lateinit var albumsAdapter: AlbumsAdapter
+
+    override fun setupOnViewCreated(view: View) {
+        bindData()
+
+        for (i in 1..100) { //TODO remove after handle viewmodel
+            list.add(Album(i, "track_$i"))
+        }
+        albumsAdapter.submitList(list)
+    }
+
+    private fun bindData() {
+        initRecyclerView()
+        loadImage("https://i.picsum.photos/id/859/200/200.jpg?hmac=vEU-8IgIt_Q7UmUqqkedPnQX0g_C-6w4WPB2VHTzfgg")
+        binding.albumName.text = "AA Album"
+        binding.albumArtistName.text = "BB Artist"
+    }
+
+    private fun initRecyclerView() {
+        binding.albumTrackList.apply {
+            albumsAdapter = AlbumsAdapter()
+            adapter = albumsAdapter
+        }
+    }
+
+    private fun loadImage(url: String?) {
+        Glide.with(requireContext()).load(url)
+            // .placeholder(R.drawable.loading_img)
+            .thumbnail(IMAGE_SIZE_MULTIPLIER)
+            .into(binding.albumCoverImage)
     }
 
 }
